5b90b7b5ce0b02fcdc93d7a89f9ec20f9e35edea
Abhinand <87251316+abzops@users.noreply.github.com>
2022-02-13 20:50:53 +0530

    Add task page design is complete now I am going to add function

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -7,177 +7,147 @@ import { useState } from "react";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import CancelIcon from "@mui/icons-material/Cancel";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import DeleteIcon from "@mui/icons-material/Delete";
 function AddTask() {
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [dlt, setDlt] = useState();
   const handleImageChange = (e) => {
-    // console.log(e.target.files[])
     if (e.target.files) {
       const filesArray = Array.from(e.target.files).map((file) =>
         URL.createObjectURL(file)
       );
 
-      // console.log("filesArray: ", filesArray);
-
       setSelectedFiles((prevImages) => prevImages.concat(filesArray));
-      Array.from(e.target.files).map(
+      Array.from(e.target.files).forEach(
         (file) => URL.revokeObjectURL(file) // avoid memory leak
       );
-      setShow(true);
     }
   };
-
-  var x = selectedFiles.length;
-  var y = x - 1;
-  const [i, setI] = useState(0);
-
-  if (i <  0) {
-    setI(0);
-  }
-  
-  if (i) {
-    if (i > y) {
-      setI(y);
-    }
-  }
-  const remove = () => {
-    
-    const all = selectedFiles;
-    all.splice(i, 1);
-    setSelectedFiles(all)
+  const [showImage, setShowImage] = useState(true);
+  const [newData, setNewData] = useState();
+  const deletePic = (key) => {
+    setNewData(selectedFiles.splice(key, 1));
   };
-  console.log(i);
-  const [show, setShow] = useState(false);
   return (
     <div>
-      <div className="pMain">
-        <Container
-          className="mainContainer "
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            padding: "20px",
-          }}
-          maxWidth="xs"
-        >
-          <div className="addTask">
-            <div className="heading">
-              <span>Add Task</span>
-              <div className="line"></div>
-            </div>
-            <div className="title">
-              <div className="input">
-                <TextField
-                  id="standard-basic"
-                  label="Title"
-                  variant="standard"
-                  size="small"
-                  color="primary"
-                  fullWidth
-                />
+      {showImage ? (
+        <div className="pMain">
+          <Container
+            className="mainContainer "
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              padding: "20px",
+            }}
+            maxWidth="xs"
+          >
+            <div className="addTask">
+              <div className="heading">
+                <span>Add Task</span>
+                <div className="line"></div>
               </div>
-            </div>
-            <div className="subject">
-              <div className="input">
-                <TextField
-                  id="standard-basic"
-                  label="subject"
-                  variant="standard"
-                  size="small"
-                  color="primary"
-                  fullWidth
-                />
-              </div>
-            </div>
-            <div className="description">
-              <div className="input">
-                <TextField
-                  multiline
-                  rows={4}
-                  label="Description"
-                  variant="standard"
-                  size="small"
-                  color="primary"
-                  fullWidth
-                />
-              </div>
-            </div>
-            <div className="addPic">
-              <div className="input">
-                <div className="ph">
-                  <span>Add Photo</span>
-                  <label htmlFor="add">
-                    <AddAPhotoIcon fontSize="medium" sx={{ color: "#666" }} />
-                  </label>
-                  <input
-                    type="file"
-                    style={{ display: "none" }}
-                    id="add"
-                    onChange={handleImageChange}
-                    multiple
+              <div className="title">
+                <div className="input">
+                  <TextField
+                    id="standard-basic"
+                    label="Title"
+                    variant="standard"
+                    size="small"
+                    color="primary"
+                    fullWidth
                   />
                 </div>
-                {show ? (
-                  <>
-                    <div
-                      className="image"
-                      style={{ backgroundImage: `url(${selectedFiles[i]})` }}
-                    >
-                      <ArrowCircleLeftIcon
-                        fontSize="medium"
-                        sx={{
-                          color: "#ef4b4c",
-                          backgroundColor: "#fff",
-                          borderRadius: "50%",
-                        }}
-                        onClick={() => {
-                          setI(i - 1);
-                        }}
-                      />
-
-                      <CancelIcon
-                        fontSize="3rem"
-                        style={{
-                          color: "#ef4b4c",
-                          margin: "0",
-                          marginBottom: "82px",
-                          marginLeft: "171px",
-                          backgroundColor: "#fff",
-                          borderRadius: "50%",
-                        }}
-                        onClick={remove}
-                      />
-                      <ArrowCircleRightIcon
-                        fontSize="medium"
-                        sx={{
-                          color: "#ef4b4c",
-                          backgroundColor: "#fff",
-                          borderRadius: "50%",
-                        }}
-                        onClick={() => {
-                          setI(i + 1);
-                        }}
+              </div>
+              <div className="subject">
+                <div className="input">
+                  <TextField
+                    id="standard-basic"
+                    label="subject"
+                    variant="standard"
+                    size="small"
+                    color="primary"
+                    fullWidth
+                  />
+                </div>
+              </div>
+              <div className="description">
+                <div className="input">
+                  <TextField
+                    multiline
+                    rows={4}
+                    label="Description"
+                    variant="standard"
+                    size="small"
+                    color="primary"
+                    fullWidth
+                  />
+                </div>
+              </div>
+              <div className="addPic">
+                <div className="input">
+                  <div className="ph">
+                    <span>Add Photo</span>
+                    <AddAPhotoIcon fontSize="medium" sx={{ color: "#666" }} onClick={()=>{setShowImage(false)}} />
+                    <span className="selected">{selectedFiles.length}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="submit">
+                <div className="buttonDiv">
+                  <input
+                    class="text-small-uppercase"
+                    id="submit"
+                    type="submit"
+                    value="Submit"
+                  />
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <div className="imageDiv">
+          <div className="imageDivChild">
+            <ArrowBackIcon fontSize="large" sx={{ color: "#666" }} onClick={()=>{setShowImage(true)}} />
+            <div className="head">
+              <h1>Add Photos</h1>
+            </div>
+            <div className="icon">
+              <label htmlFor="add">
+                <AddAPhotoIcon fontSize="large" sx={{ color: "#666" }} />
+              </label>
+              <input
+                type="file"
+                style={{ display: "none" }}
+                id="add"
+                onChange={handleImageChange}
+                multiple
+              />
+            </div>
+            <hr />
+            <div className="images">
+              {selectedFiles.map((img, index) => {
+                return (
+                  <div className="imagesChild" key={index}>
+                    <img src={img} />
+                    <div className="deleteIcon">
+                      <DeleteIcon
+                        sx={{ color: "#666" }}
+                        onClick={() => deletePic(index)}
                       />
                     </div>
-                  </>
-                ) : null}
-              </div>
+                  </div>
+                );
+              })}
             </div>
-            <div className="submit">
-              <div className="buttonDiv">
-                <input
-                  class="text-small-uppercase"
-                  id="submit"
-                  type="submit"
-                  value="Submit"
-                />
-              </div>
-            </div>
+
+            <button>Submit</button>
           </div>
-        </Container>
-      </div>
+        </div>
+      )}
+
       <Navbar />
     </div>
   );
